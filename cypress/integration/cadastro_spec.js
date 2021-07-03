e28df01e35b1a13fdb6dbc4686962a6d7565e4ba
tiagopelegrini <tiago.pelegrini@gmail.com>
2021-07-03 18:36:09 -0300

    asserts e mocks

--- cypress/integration/cadastro_spec.js
+++ cypress/integration/cadastro_spec.js
@@ -12,16 +12,30 @@ context('Cadastro', () => {
         //POST (aborted) /api/1/databases/userdetails/collections/usertable?apiKey=YEX0M2QMPd7JWJw_ipMB3a5gDddt4B_X
 
         //GET (aborted) /api/1/databases/userdetails/collections/newtable?apiKey=YEX0M2QMPd7JWJw_ipMB3a5gDddt4B_X
-        cy.server()
-        cy.route('POST', '**/api/1/databases/userdetails/collections/newtable?**')
-            .as('postNewtable');//Apelidos
+       //Rotas
 
 
-        cy.route('POST', '**/api/1/databases/userdetails/collections/usertable?**')
-            .as('postUsertable');
+       cy.server()
+       cy.route({
+       method: 'POST',
+       url: '**/api/1/databases/userdetails/collections/newtable?**',
+       status: 200,
+       response: {}
+       }).as('postNewtable');
 
-        cy.route('GET', '**/api/1/databases/userdetails/collections/newtable?**')
-            .as('getNewtable');
+       cy.route({
+           method: 'POST', 
+           url: '**/api/1/databases/userdetails/collections/usertable?**', 
+           status: 200, 
+           response: {}
+       }).as('postUsertable');
+
+       cy.route({
+       method: 'GET',
+       url: '**/api/1/databases/userdetails/collections/newtable?**',
+       status: 200,
+       response: {}
+       }).as('getNewtable');
 
         // baseUrl + register.html
         cy.visit('Register.html');
@@ -61,7 +75,21 @@ context('Cadastro', () => {
             console.log(resNewtable.status)
             cy.log(resNewtable.status)
 
+            //chai
+            expect(resNewtable.status).to.eq(200)
         })
+
+        cy.wait('@postUsertable').then((resUsertable) => {
+            //chai
+            expect(resUsertable.status).to.eq(200)
+        })
+
+        cy.wait('@getNewtable').then((resgetNewtable) => {
+            expect(resgetNewtable.status).to.eq(200)
+        })
+
+        cy.url().should('contain', 'WebTable');
+
     });
 });
 
